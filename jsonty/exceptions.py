359b55fcc75a96a2c4564a570f5d2e840dfb6a13
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2019-11-17 17:13:24 +0100

    feat(dumps): list and set iterable dumps without type warning (#2)

--- jsonty/exceptions.py
+++ jsonty/exceptions.py
@@ -1,3 +1,6 @@
 
 class TypeNotReconized(Exception):  
      """Raised when a type is not reconized"""
+
+class IterableNotAllSameType(Exception):
+     """ Raised when an interable does not have the same typed values """
---
+++ tests/test_dumps/test_iteration.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+from ..context import jsonty
+
+import unittest
+
+import json
+
+class InterationDump(unittest.TestCase):
+    """Dump test operation with simple attributes via annotations"""
+
+    class Subject(jsonty.Model):
+        name: str
+        grade: int
+
+        def __init__(self, name: str, grade: int):
+            self.name = name
+            self.grade = grade
+
+    class Student(jsonty.Model):
+        name: str
+        age: int
+        subjects: list
+
+        def __init__(self, name: str, age: int, subjects: list):
+            self.name = name
+            self.age = age
+            self.subjects = subjects
+    
+
+    def test_subject(self):
+        """ Check that the Subject class constructor works """
+        # Args
+        name = 'Maths'
+        grade = 1
+        # Object construction
+        obj = self.Subject(name=name, grade=grade)
+        # Constructor class works
+        self.assertIsNotNone(obj)
+        # Check attributes
+        self.assertEqual(name, obj.name)
+        self.assertEqual(grade, obj.grade)
+
+    def test_subject_dump(self):
+        """ Check that the Subject class dumps works """
+        # Args
+        name = 'Maths'
+        grade = 1
+        # Object construction
+        obj = self.Subject(name=name, grade=grade)
+        # Expected result
+        expected = json.dumps({
+            'name': name,
+            'grade': grade
+        })
+
+        # Dumps operation
+        res = obj.dumps()
+
+        self.assertEqual(res, expected)
+
+
+    def test_student(self):
+        """ Check that the Student class constructor works """
+        # Args
+        name = 'Mathew'
+        age = 15
+        subjects = [self.Subject(name='Maths', grade=1), self.Subject(name='English', grade=1)]
+        # Object construction
+        obj = self.Student(name=name, age=age, subjects=subjects)
+        # Constructor class works
+        self.assertIsNotNone(obj)
+        # Check attributes
+        self.assertEqual(name, obj.name)
+        self.assertEqual(age, obj.age)
+        self.assertEqual(subjects, obj.subjects)
+
+
+    def test_list_model(self):
+        """ Check that the Student with an iterable of a model class can be dumped """
+        # Args
+        name = 'Mathew'
+        age = 15
+        s1 = self.Subject(name='Maths', grade=1)
+        s2 = self.Subject(name='English', grade=1)
+        subjects = [s1, s2]
+        # Object construction
+        obj = self.Student(name=name, age=age, subjects=subjects)
+        # Expected result
+        expected = json.dumps({
+            'name': name,
+            'age': age,
+            'subjects': [
+                {
+                    'name': s1.name,
+                    'grade': s1.grade
+                },
+                {
+                    'name': s2.name,
+                    'grade': s2.grade
+                }
+            ]
+        })
+        
+        # Dumps operation
+        res = obj.dumps()
+        
+        self.assertEqual(res, expected)
+
+if __name__ == '__main__':
+    unittest.main()
